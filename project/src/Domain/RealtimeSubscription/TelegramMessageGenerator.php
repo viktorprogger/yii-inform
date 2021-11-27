17621fd87e3283fdaec61d7b07b7d601f4514d82
viktorprogger <viktorprogger@gmail.com>
2021-11-28 01:25:57 +0300

    GitHub event message generating

--- project/src/Domain/RealtimeSubscription/TelegramMessageGenerator.php
+++ project/src/Domain/RealtimeSubscription/TelegramMessageGenerator.php
@@ -18,125 +18,137 @@ final class TelegramMessageGenerator
     {
     }
 
-    public function generateForEvent(GithubEvent $subscriptionEvent, Subscriber $subscriber): TelegramMessage
+    public function generateForEvent(GithubEvent $event, Subscriber $subscriber): TelegramMessage
     {
         // FIXME take a Subscriber instead of chatId to get method newRepoCreated() done
-        return match ($subscriptionEvent->type) {
-            EventType::ISSUE_OPENED => $this->issueCreated($subscriptionEvent->payload, $subscriber->chatId),
-            EventType::ISSUE_CLOSED => $this->issueClosed($subscriptionEvent->payload, $subscriber->chatId),
-            EventType::ISSUE_REOPENED => $this->issueReopened($subscriptionEvent->payload, $subscriber->chatId),
-            EventType::ISSUE_COMMENTED => $this->issueCommented($subscriptionEvent->payload, $subscriber->chatId),
-            EventType::PR_OPENED => $this->prOpened($subscriptionEvent->payload, $subscriber->chatId),
-            EventType::PR_CLOSED => $this->prClosed($subscriptionEvent->payload, $subscriber->chatId),
-            EventType::PR_MERGED => $this->prMerged($subscriptionEvent->payload, $subscriber->chatId),
-            EventType::PR_REOPENED => $this->prReopened($subscriptionEvent->payload, $subscriber->chatId),
-            EventType::PR_CHANGED => $this->prChanged($subscriptionEvent->payload, $subscriber->chatId),
-            EventType::PR_COMMENTED => $this->prCommented($subscriptionEvent->payload, $subscriber->chatId),
-            EventType::PR_MERGE_APPROVED => $this->prMergeApproved($subscriptionEvent->payload, $subscriber->chatId),
-            EventType::PR_MERGE_DECLINED => $this->prMergeDeclined($subscriptionEvent->payload, $subscriber->chatId),
-            EventType::NEW_REPO => $this->newRepoCreated($subscriptionEvent->payload, $subscriber->chatId),
+        return match ($event->type) {
+            EventType::ISSUE_OPENED => $this->issueCreated($event, $subscriber->chatId),
+            EventType::ISSUE_CLOSED => $this->issueClosed($event, $subscriber->chatId),
+            EventType::ISSUE_REOPENED => $this->issueReopened($event, $subscriber->chatId),
+            EventType::ISSUE_COMMENTED => $this->issueCommented($event, $subscriber->chatId),
+            EventType::PR_OPENED => $this->prOpened($event, $subscriber->chatId),
+            EventType::PR_CLOSED => $this->prClosed($event, $subscriber->chatId),
+            EventType::PR_MERGED => $this->prMerged($event, $subscriber->chatId),
+            EventType::PR_REOPENED => $this->prReopened($event, $subscriber->chatId),
+            EventType::PR_CHANGED => $this->prChanged($event, $subscriber->chatId),
+            EventType::PR_COMMENTED => $this->prCommented($event, $subscriber->chatId),
+            EventType::PR_MERGE_APPROVED => $this->prMergeApproved($event, $subscriber->chatId),
+            EventType::PR_MERGE_DECLINED => $this->prMergeDeclined($event, $subscriber->chatId),
+            EventType::NEW_REPO => $this->newRepoCreated($event, $subscriber->chatId),
         };
     }
 
-    private function issueCreated(array $payload, string $chatId): TelegramMessage
+    private function issueCreated(GithubEvent $event, string $chatId): TelegramMessage
     {
-        $text = "{repo_name}: был создан тикет [{issue_name}]({issue_link}).";
+        $text = "#$event->repo\n";
+        $text .= "Был создан тикет [{issue_name}]({issue_link}).";
 
         return new TelegramMessage($text, MessageFormat::markdown(), $chatId);
     }
 
-    private function issueClosed(array $payload, string $chatId): TelegramMessage
+    private function issueClosed(GithubEvent $event, string $chatId): TelegramMessage
     {
-        $text = "{repo_name}: закрыт тикет [{issue_name}]({issue_link}).";
+        $text = "#$event->repo\n";
+        $text .= "Закрыт тикет [{issue_name}]({issue_link}).";
 
         return new TelegramMessage($text, MessageFormat::markdown(), $chatId);
     }
 
-    private function issueReopened(array $payload, string $chatId): TelegramMessage
+    private function issueReopened(GithubEvent $event, string $chatId): TelegramMessage
     {
-        $text = "{repo_name}: заново открыт тикет [{issue_name}]({issue_link}).";
+        $text = "#$event->repo\n";
+        $text .= "Заново открыт тикет [{issue_name}]({issue_link}).";
 
         return new TelegramMessage($text, MessageFormat::markdown(), $chatId);
     }
 
-    private function issueCommented(array $payload, string $chatId): TelegramMessage
+    private function issueCommented(GithubEvent $event, string $chatId): TelegramMessage
     {
         $text = <<<MD
-            {repo_name}: в тикет [{issue_name}]({issue_link}) добавлен комментарий
+            #$event->repo
+            В тикет [{issue_name}]({issue_link}) добавлен комментарий
             _{comment_text}_
             MD;
 
         return new TelegramMessage($text, MessageFormat::markdown(), $chatId);
     }
 
-    private function prOpened(array $payload, string $chatId): TelegramMessage
+    private function prOpened(GithubEvent $event, string $chatId): TelegramMessage
     {
-        $text = "{repo_name}: открыт PR [{pr_name}]({pr_link}).";
+        $text = "#$event->repo\n";
+        $text .= "Открыт PR [{pr_name}]({pr_link}).";
 
         return new TelegramMessage($text, MessageFormat::markdown(), $chatId);
     }
 
-    private function prClosed(array $payload, string $chatId): TelegramMessage
+    private function prClosed(GithubEvent $event, string $chatId): TelegramMessage
     {
-        $text = "{repo_name}: закрыт PR [{pr_name}]({pr_link}).";
+        $text = "#$event->repo\n";
+        $text .= "Закрыт PR [{pr_name}]({pr_link}).";
 
         return new TelegramMessage($text, MessageFormat::markdown(), $chatId);
     }
 
-    private function prMerged(array $payload, string $chatId): TelegramMessage
+    private function prMerged(GithubEvent $event, string $chatId): TelegramMessage
     {
-        $text = "{repo_name}: смержили PR [{pr_name}]({pr_link}).";
+        $text = "#$event->repo\n";
+        $text .= "Смержили PR [{pr_name}]({pr_link}).";
 
         return new TelegramMessage($text, MessageFormat::markdown(), $chatId);
     }
 
-    private function prReopened(array $payload, string $chatId): TelegramMessage
+    private function prReopened(GithubEvent $event, string $chatId): TelegramMessage
     {
-        $text = "{repo_name}: заново открыт PR [{pr_name}]({pr_link}).";
+        $text = "#$event->repo\n";
+        $text .= "Заново открыт PR [{pr_name}]({pr_link}).";
 
         return new TelegramMessage($text, MessageFormat::markdown(), $chatId);
     }
 
-    private function prChanged(array $payload, string $chatId): TelegramMessage
+    private function prChanged(GithubEvent $event, string $chatId): TelegramMessage
     {
         $text = <<<MD
-            {repo_name}: в PR [{pr_name}]({pr_link}) произошли изменения.
+            #$event->repo
+             В PR [{pr_name}]({pr_link}) произошли изменения.
             {changes_summary}
             MD;
 
         return new TelegramMessage($text, MessageFormat::markdown(), $chatId);
     }
 
-    private function prCommented(array $payload, string $chatId): TelegramMessage
+    private function prCommented(GithubEvent $event, string $chatId): TelegramMessage
     {
         $text = <<<MD
-            {repo_name}: в PR [{pr_name}]({pr_link}) добавлен комментарий
+            #$event->repo
+             В PR [{pr_name}]({pr_link}) добавлен комментарий
             {PR_comment}
             MD;
 
         return new TelegramMessage($text, MessageFormat::markdown(), $chatId);
     }
 
-    private function prMergeApproved(array $payload, string $chatId): TelegramMessage
+    private function prMergeApproved(GithubEvent $event, string $chatId): TelegramMessage
     {
-        $text = "{repo_name}: Одобрен мёрж пулл реквеста [{pr_name}]({pr_link}).";
+        $text = "#$event->repo\n";
+        $text .= "Одобрен мёрж пулл реквеста [{pr_name}]({pr_link}).";
 
         return new TelegramMessage($text, MessageFormat::markdown(), $chatId);
     }
 
-    private function prMergeDeclined(array $payload, string $chatId): TelegramMessage
+    private function prMergeDeclined(GithubEvent $event, string $chatId): TelegramMessage
     {
-        $text = "{repo_name}: для пулл реквеста [{pr_name}]({pr_link}) требуются изменения.";
+        $text = "#$event->repo\n";
+        $text .= "Для пулл реквеста [{pr_name}]({pr_link}) требуются изменения.";
 
         return new TelegramMessage($text, MessageFormat::markdown(), $chatId);
     }
 
-    private function newRepoCreated(array $payload, string $chatId): TelegramMessage
+    private function newRepoCreated(GithubEvent $event, string $chatId): TelegramMessage
     {
-        $text = "Создан новый репозиторий: {repo_name}";
+        $text = "Создан новый репозиторий $event->repo";
         $keyboard = [[
-            $this->formatter->createInlineButton('{repo_name}', ButtonAction::ADD, SubscriptionType::REALTIME, 'Подписаться realtime'),
-            $this->formatter->createInlineButton('{repo_name}', ButtonAction::ADD, SubscriptionType::SUMMARY, 'Подписаться на summary'),
+            $this->formatter->createInlineButton($event->repo, ButtonAction::ADD, SubscriptionType::REALTIME, 'Подписаться realtime'),
+            $this->formatter->createInlineButton($event->repo, ButtonAction::ADD, SubscriptionType::SUMMARY, 'Подписаться на summary'),
         ]];
 
         return new TelegramMessage($text, MessageFormat::markdown(), $chatId, $keyboard);
