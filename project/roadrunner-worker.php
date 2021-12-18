cb41b3ea1df0732e304e22a22296887bcb5a0b53
viktorprogger <viktorprogger@gmail.com>
2021-12-18 21:30:14 +0300

    Fix config

--- project/roadrunner-worker.php
+++ project/roadrunner-worker.php
@@ -2,9 +2,28 @@
 
 declare(strict_types=1);
 
+use Yiisoft\Config\Config;
+use Yiisoft\Config\ConfigPaths;
+use Yiisoft\Config\Modifier\RecursiveMerge;
+use Yiisoft\Config\Modifier\RemoveFromVendor;
+use Yiisoft\Config\Modifier\ReverseMerge;
 use Yiisoft\Yii\Runner\RoadRunner\RoadRunnerApplicationRunner;
 
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/preload.php';
 
-(new RoadRunnerApplicationRunner(__DIR__, true, null))->run();
+$environment = null;
+
+(new RoadRunnerApplicationRunner(__DIR__, true, $environment))
+    ->withConfig(
+        new Config(
+            new ConfigPaths(__DIR__, 'config'),
+            $environment,
+            [
+                ReverseMerge::groups('events', 'events-web', 'events-console'),
+                RecursiveMerge::groups('params', 'events', 'events-web', 'events-console'),
+                RemoveFromVendor::keys(['yiisoft/log-target-file', 'fileTarget', 'levels']),
+            ],
+        )
+    )
+    ->run();
