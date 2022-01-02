72e68b91870ee788864d3c77d62935448a4d1c08
viktorprogger <viktorprogger@gmail.com>
2022-01-02 04:36:50 +0300

    Preparing to production

--- project/config/routes.php
+++ project/config/routes.php
@@ -3,12 +3,16 @@
 declare(strict_types=1);
 
 use Viktorprogger\YiisoftInform\SubDomain\Telegram\Infrastructure\Web\Controller\TelegramHookController;
+use Yiisoft\DataResponse\DataResponseFactoryInterface;
 use Yiisoft\DataResponse\Middleware\FormatDataResponseAsJson;
 use Yiisoft\Request\Body\RequestBodyParser;
 use Yiisoft\Router\Route;
 use Yiisoft\Yii\Sentry\SentryMiddleware;
 
 return [
+    Route::get('/')
+        ->middleware(FormatDataResponseAsJson::class)
+        ->action(static fn (DataResponseFactoryInterface $responseFactory) => $responseFactory->createResponse()),
     Route::post('/telegram/hook')
         ->middleware(SentryMiddleware::class)
         ->middleware(RequestBodyParser::class)
