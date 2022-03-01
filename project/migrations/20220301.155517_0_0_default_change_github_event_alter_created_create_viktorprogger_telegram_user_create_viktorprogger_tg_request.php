8557f78e98700fa399fe26483323d64d43c82a21
viktorprogger <viktorprogger@gmail.com>
2022-03-01 19:08:35 +0300

    Add migration and drop redundant entity

---
+++ project/migrations/20220301.155517_0_0_default_change_github_event_alter_created_create_viktorprogger_telegram_user_create_viktorprogger_tg_request.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Viktorprogger\YiisoftInform\Migration;
+
+use Spiral\Migrations\Migration;
+
+class OrmDefaultAeea5c0bccf2e49e536823d6c99eced8 extends Migration
+{
+    protected const DATABASE = 'default';
+
+    public function up(): void
+    {
+        $this->table('viktorprogger_telegram_user')
+            ->addColumn('id', 'string', [
+                'nullable' => false,
+                'default' => null,
+                'size' => 255,
+            ])
+            ->setPrimaryKeys(["id"])
+            ->create();
+
+        $this->table('tg_update')
+            ->rename('viktorprogger_tg_request');
+    }
+
+    public function down(): void
+    {
+        $this->table('viktorprogger_tg_request')->rename('tg_update');
+        $this->table('viktorprogger_telegram_user')->drop();
+    }
+}
